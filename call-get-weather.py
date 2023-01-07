42da6df280a56bad4d5793d4dcb31e384590810a
uneqorn <yashaharari@gmail.com>
2023-01-07 13:35:05 +0200

    Update call-get-weather.py

--- call-get-weather.py
+++ call-get-weather.py
@@ -16,3 +16,9 @@ def get_weather_ajax():
   
   # return the weather data as a JSON object
   return jsonify(weather_data)
+
+
+# This script will create a Flask app with a route that handles POST requests to the /get-weather URL. The route gets the location from the request and calls the get_weather() function, then returns the weather data as a JSON object using the jsonify() function from Flask.
+# You will need to make sure that the get_weather() function is available to this script. You can do this by placing the function in a separate module and importing it, or by defining it in the same script.
+# To use this script, you will need to run it on a server and make sure that it is accessible from your website. You can then send an AJAX request to the /get-weather URL from your JavaScript code and handle the response by updating the page with the weather data.
+# I hope this is helpful! Please let me know if you have any questions or if you'd like more information.
